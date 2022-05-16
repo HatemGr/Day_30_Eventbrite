19db12fa2c1e2e1398d36a0b40029b820ddd487e
HatemGr <hatemgribi@gmail.com>
2022-05-16 10:30:28 +0200

    change mailjet port

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :password => ENV['MAILJET_SECRET_KEY'],
   :domain => 'thpevent.com',
   :address => 'in-v3.mailjet.com',
-  :port => 587,
+  :port => 465,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
